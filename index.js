b5ea8c79bab175462fef05bf986134e3b36ed79c
Melissa Rudick <mel.madrugada@gmail.com>
2017-08-22 03:45:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,12 @@ function destructivelyRemoveFirstKitten() {
   kittens.shift()
   return kittens
 }
-function appendKitten(name) {
-  return [kittens, ...name]
-}
+// function appendKitten(name) {
+//   return [kittens, ...name]
+// }
 function prependKitten(name) {
   return [name, ...kittens]
+}
+function removeLastKitten() {
+  return [kittens, ...name]
 }
